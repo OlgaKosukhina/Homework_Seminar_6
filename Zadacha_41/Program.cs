5d03926cc23dd0b49a2ebd766e23832a1f4d30f5
Olga Kosukhina <heeeelga@gmail.com>
2022-07-14 14:33:46 +0300

    Solved tasks

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int[] GetArray(int massiveLength)
+{
+    int[] array = new int[massiveLength];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Insert massive element: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+int GetCountOfPositive(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Input massive length: ");
+int massiveLength = int.Parse(Console.ReadLine());
+int[] arr = GetArray(massiveLength);
+int result = GetCountOfPositive(arr);
+Console.WriteLine($"Amount of positive numbers: {result}");
---
+++ Zadacha_43/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2.
+Значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+*/
+
+int GetNumber()
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine("Insert number:");
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("You input not a number. Please input correct number.");
+        }
+    }
+    return result;
+}
+
+(double, double) GetPointOfIntersection(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * (b2 - b1) / (k1 - k2)) + b1;
+
+    return (x, y);
+}
+
+
+double b1 = GetNumber();
+double k1 = GetNumber();
+double b2 = GetNumber();
+double k2 = GetNumber();
+(double x, double y) = GetPointOfIntersection(b1, k1, b2, k2);
+Console.WriteLine($"The point of intersection has coordinates ({x};{y})");
